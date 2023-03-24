aa5af4ce0d4b987a6c03ba532802c05b1cf89a8d
Jacqueline Quissi <jacqueline.quissi@hotmail.com>
2023-03-24 11:19:33 -0300

    primeiro commit

---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+
+WITH src_reviews AS (
+    SELECT * FROM {{ ref ('src_reviews') }}
+)
+SELECT 
+    listing_id, 
+    review_date, 
+    reviewer_name, 
+    review_text, 
+    review_sentiment
+FROM src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+  and review_date > (select max(review_date) from {{ this }})
+{% endif %}
